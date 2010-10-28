5d1cc28968abd647d9c18fe877d80e82d9665080
Lukas Kahwe Smith <smith@pooteeweet.org>
2010-10-28 09:51:13 +0200

    added MIT license

--- LiipMultiplexBundle.php
+++ LiipMultiplexBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Liip/MultiplexBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Bundle\Liip\MultiplexBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
