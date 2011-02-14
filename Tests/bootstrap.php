8ed2f3b7ec3fb67dea483d3b2be68eb9f2d78ae2
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-02-14 19:48:40 +0100

    updated to match new class structure

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/HttpFoundation/UniversalClassLoader.php';
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\HttpFoundation\UniversalClassLoader;
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
@@ -10,8 +10,8 @@ $loader->register();
 
 spl_autoload_register(function($class)
 {
-    if (0 === strpos($class, 'Bundle\\Liip\\MultiplexBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 3)).'.php';
+    if (0 === strpos($class, 'Liip\\MultiplexBundle\\')) {
+        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
         require_once __DIR__.'/../'.$path;
         return true;
     }
